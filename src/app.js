dc334baa414852c8258bdeb688e2dc69696d3f50
Ates Goral <ates@magnetiq.com>
2016-04-12 13:11:49 -0400

    Refresh with countdown, token display and verification

--- src/app.js
+++ src/app.js
@@ -6,19 +6,32 @@ const qr = require('qr-image');
 
 const app = express();
 
-app.use('/', express.static('static'));
+const secret = speakeasy.generateSecret();
+
+app.use('/', express.static(__dirname + '/static'));
 
 app.get('/qrcode', (req, res) => {
-  const secret = speakeasy.generateSecret();
-
   const token = speakeasy.totp({
     secret: secret.base32,
-    encoding: 'base32'
+    encoding: 'base32',
+    step: 30
   });
 
-  res.cookie('timeStep', 30);
+  res.cookie('token', token);
 
-  qr.image('http://example.com/nudge/' + token).pipe(res);
+  qr.image(req.query.baseUrl + 'verify/' + token, { margin: 0 }).pipe(res);
+});
+
+app.get('/verify/:token', (req, res) => {
+  const isValid = speakeasy.totp.verify({
+    secret: secret.base32,
+    encoding: 'base32',
+    step: 30,
+    window: 1,
+    token: req.params.token
+  });
+
+  res.send(isValid);
 });
 
 module.exports = app;
