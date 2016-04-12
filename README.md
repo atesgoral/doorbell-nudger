303360253e543ce9f7260e9196ab884304aa6fc4
Ates Goral <ates@magnetiq.com>
2016-04-12 15:11:32 -0400

    Brought in Twitter

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # doorbell-nudger
+
+## Setup
+
+Copy .env.example to .env and fill in Twitter API keys/secrets.
+
+Install dependencies:
+
+```sh
+npm install
+```
+
+Then run:
+
+```sh
+bin/www
+```
--- src/app.js
+++ src/app.js
@@ -3,10 +3,18 @@
 const express = require('express');
 const speakeasy = require('speakeasy');
 const qr = require('qr-image');
+const Twitter = require('twitter');
 
 const app = express();
 
 const secret = speakeasy.generateSecret();
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 app.use('/', express.static(__dirname + '/static'));
 
