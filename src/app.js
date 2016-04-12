604c66f5976f5f3e8f4b319dad156a1f67f2e708
Ates Goral <ates.g@myplanet.com>
2016-04-12 16:27:09 -0400

    Non-mentionize tweet

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.get('/verify/:token', (req, res) => {
   if (isValid) {
     console.log('Token is valid');
 
-    client.post('statuses/update', { status: `@DoorbellRinger Token ${ req.params.token } is valid. #ringit` }, (error) => {
+    client.post('statuses/update', { status: `Hey @DoorbellRinger, token ${ req.params.token } is valid. #ringit` }, (error) => {
       if (error) {
         console.error('Tweet failed', error);
       } else {
